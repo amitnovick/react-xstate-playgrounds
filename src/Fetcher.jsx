e69ca5f105aa95dad0742bad45c8def0cecb94c3
feelextra <felxtra@gmail.com>
2019-04-01 02:38:19 +0300

    Implemented pure machine config, all side effects / computations are done outside

--- src/Fetcher.jsx
+++ src/Fetcher.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useMachine } from '@xstate/react';
-import { Machine, assign } from 'xstate';
+import { Machine } from 'xstate';
 import axios from 'axios';
 
 const RESPONSE_NOT_OK = 'notOK';
@@ -22,74 +22,48 @@ const fetchUser = query =>
       }
     });
 
-const fetchMachine = Machine(
-  {
-    id: 'fetch',
-    initial: 'idle',
-    context: {
-      user: null,
-      error: null
+const fetchMachine = Machine({
+  id: 'fetch',
+  initial: 'idle',
+  states: {
+    idle: {
+      on: { FETCH: 'loading' }
     },
-    states: {
-      idle: {
-        on: { FETCH: 'loading' }
-      },
-      loading: {
-        invoke: {
-          src: (ctx, event) => fetchUser(event.query),
-          onDone: {
-            target: 'success',
-            actions: assign({
-              user: (_, event) => event.data
-            })
-          },
-          onError: {
-            target: 'failure',
-            actions: assign({ error: (ctx, event) => event.data.message })
-          }
-        }
-      },
-      success: {
-        onEntry: 'notifyResolve',
-        type: 'final'
-      },
-      failure: {
-        onEntry: 'notifyResolve',
-        type: 'final'
-      }
-    }
-  },
-  {
-    actions: {
-      notifyResolve: (ctx, event) => console.log('notify', 'ctx:', ctx)
+    loading: {
+      on: { FETCH_SUCCEEDED: 'success', FETCH_FAILED: 'failure' }
+    },
+    success: {
+      type: 'final'
+    },
+    failure: {
+      type: 'final'
     }
   }
-);
+});
 
 function Fetcher({ query }) {
   const [current, send] = useMachine(fetchMachine);
 
+  const onButtonClick = query => {
+    send({ type: 'FETCH' });
+    return fetchUser(query)
+      .then(data => send({ type: 'FETCH_SUCCEEDED', outcome: 'success' }))
+      .catch(error => send({ type: 'FETCH_FAILED', outcome: 'failure' }));
+  };
+
   switch (current.value) {
     case 'idle':
       return (
-        <button onClick={() => send({ type: 'FETCH', query: query })}>
+        <button onClick={() => onButtonClick(query)}>
           Search for something
         </button>
       );
     case 'loading':
       return <div>Searching...</div>;
     case 'success':
-      return <div>Success! Data: {JSON.stringify(current.context.user)}</div>;
+      return <div>Success!</div>;
     case 'failure':
-      const errorType = current.context.error;
-      switch (errorType) {
-        case RESPONSE_NOT_OK:
-          return <div>Failed. service is unavailable.</div>;
-        case CLIENT_DISCONNECTED:
-          return <div>Failed. please reconnect and try again. </div>;
-        default:
-          return null;
-      }
+      return <div>Failed. :-(</div>;
     default:
       return null;
   }
